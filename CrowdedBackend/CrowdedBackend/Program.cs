011e50bdc11c51bd61c56445880129801e3d1687
davidfraenkel <david.fraenkel@hotmail.com>
2025-02-26 17:26:24 +0100

    added database connection

---
+++ CrowdedBackend/CrowdedBackend/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// Configure PostgreSQL database connection
+builder.Services.AddDbContext<MyDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Configure the HTTP request pipeline.
+
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.Run();
