326862a5f8d0bcea3a7c7b322b5c3f14238d9005
Anders <andersraae01@gmail.com>
2025-03-10 11:23:57 +0100

    CRO-10 base

---
+++ CrowdedBackend/CrowdedBackend/Models/RaspData.cs
@@ -0,0 +1,10 @@
+namespace DefaultNamespace;
+
+public class RaspData
+{
+    public int id { get; set; }
+    public string macAddress { get; set; }
+    public string signalStrengthRSSI { get; set; }  // kan det blive en int???
+    public string company { get; set; }
+    public int timestamp { get; set; } // 
+}
