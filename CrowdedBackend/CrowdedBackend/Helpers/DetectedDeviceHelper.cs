1d41dbcb0e931b95d8d99b2be2eaedc6992bc4d1
Anders <andersraae01@gmail.com>
2025-04-29 09:50:32 +0200

    CRO-71 formatting

--- CrowdedBackend/CrowdedBackend/Helpers/DetectedDeviceHelper.cs
+++ CrowdedBackend/CrowdedBackend/Helpers/DetectedDeviceHelper.cs
@@ -17,13 +17,13 @@ public class DetectedDeviceHelper
         this._context = context;
         this._circleUtils = circleUtils;
     }
-  
+
     public async Task<RaspOutputData> HandleRaspPostRequest(RaspOutputData raspOutputData, long now)
     {
         try
         {
             var raspberryPi = await _context.RaspberryPi.FindAsync(raspOutputData.Id);
-          
+
             Console.WriteLine($"This is raspberryPi: {raspberryPi}");
 
             if (raspberryPi == null)
