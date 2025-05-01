060a895c59dd9e80046de8b49f295174690baa9b
Anders <andersraae01@gmail.com>
2025-05-01 11:16:00 +0200

    CRO-80 fix for devices logic and removed console logs

--- CrowdedBackend/CrowdedBackend/Controllers/VenueController.cs
+++ CrowdedBackend/CrowdedBackend/Controllers/VenueController.cs
@@ -72,7 +72,6 @@ namespace CrowdedBackend.Controllers
         [HttpPost]
         public async Task<ActionResult<Venue>> PostVenue(Venue venue)
         {
-            Console.Out.WriteLine(venue);
             _context.Venue.Add(venue);
             await _context.SaveChangesAsync();
 
